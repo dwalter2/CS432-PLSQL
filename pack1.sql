65e2da9493a4a91e171dfbb44f8471144e6635df
dwalter2 <32689604+dwalter2@users.noreply.github.com>
2020-10-18 13:06:04 -0400

    Update pack1.sql

--- pack1.sql
+++ pack1.sql
@@ -283,9 +283,9 @@ insert into courses values('CS',240,'Data Structures');
 insert into classes values('c0003','CS',240,01,2020,'Fall',100,99);
 exec show_all_enrolled('c0003');
 
-create or replace procedure enroll_student(studentid in students.sid%type, cid in classes.classid%type) is 
+create or replace procedure enroll_student(studentid in students.sid%type, cid in classes.classid%type) is
   begin
-  declare 
+  declare
     prereq_taken_count number(2);
     prereq_total_count number(2);
     class_count number(2);
@@ -295,11 +295,11 @@ create or replace procedure enroll_student(studentid in students.sid%type, cid i
     class_cn classes.course_no%type;
     cursor c1 is select sid from students where sid = studentid;
     c1_rec c1%rowtype;
-    cursor c2 is select classid from classes where classid = cid;
+    cursor c2 is select classid,limit,class_size from classes where classid = cid;
     c2_rec c2%rowtype;
     cursor c3 is select classid from classes where classid = cid and classid in (
       select classid from enrollments where sid = studentid
-    ); 
+    );
     c3_rec c3%rowtype;
     cursor c4 is select * from enrollments where sid = studentid and classid in (
       select classid from classes where classid = cid
@@ -314,8 +314,7 @@ create or replace procedure enroll_student(studentid in students.sid%type, cid i
       select classid from enrollments where sid = studentid and lgrade = 'A' or lgrade = 'B' or lgrade = 'C'
     ) and (dept_code,course_no) in (
       select pre_dept_code,pre_course_no from prerequisites where dept_code = class_dc and course_no = class_cn);
-    )
-    select count(*) into prereq_total_count from prerequisites where dept_code = class_dc and course_no = class_cn);
+    select count(*) into prereq_total_count from prerequisites where dept_code = class_dc and course_no = class_cn;
     if(not c1%isopen) then
       open c1;
     end if;
@@ -328,22 +327,146 @@ create or replace procedure enroll_student(studentid in students.sid%type, cid i
     fetch c1 into c1_rec;
     fetch c2 into c2_rec;
     fetch c3 into c3_rec;
-    if c1%notfound then 
+    if c1%notfound then
       dbms_output.put_line('sid not found');
-    elsif c2%notfound then 
+    elsif c2%notfound then
       dbms_output.put_line('The classid in invalid');
-    elsif c2_rec.limit == c2_rec.class_size then 
+    elsif c2_rec.limit = c2_rec.class_size then
       dbms_output.put_line('The class is full');
-    elsif c3%found then 
+    elsif c3%found then
       dbms_output.put_line('The student is already enrolled in this class');
-    elsif class_count > 2 then 
+    elsif class_count > 2 then
       dbms_output.put_line('You are overloaded');
-    elsif prereq_taken_count != prereq_total_count then 
+    elsif prereq_taken_count != prereq_total_count then
       dbms_output.put_line('Prerequisite courses have not been completed');
-    else 
-      insert into enrollments values(studentid,cid,'I')
+    else
+      insert into enrollments values(studentid,cid,'I');
       dbms_output.put_line('Student successfully enrolled');
     end if;
   end;
 end;
 /
+
+/*create or replace trigger trig2
+  before insert on enrollments
+  declare
+    cursor c1 is select * from logs where time = max(time) and operation = 'insert' and table_name = 'enrollments';
+    c1_rec c1%rowtype;
+  begin
+    if(not c1%isopen) then
+      open c1;
+    end if;
+    fetch c1 into c1_rec;
+    if c1%found then
+      update classes
+        set class_size = class_size + 1
+        where classid = c1_rec.key_value.classid;
+    end if;
+  end;
+/
+*/
+insert into classes values('c0002','MATH',327,1,2020,'Fall',100,20);
+insert into courses values('CS',101,'Ethics');
+insert into classes values('c0004','CS',101,1,2020,'Fall',60,1);
+insert into enrollments values('B004','c0004',null);
+insert into enrollments values('B001','c0002','B');
+
+create or replace procedure drop_student(stid in students.sid%type, clid in classes.classid%type) is
+  begin
+  declare
+    num_students number(3);
+    num_classes number(1);
+    cnum courses.course_no%type;
+    dc courses.dept_code%type;
+    cnum2 courses.course_no%type;
+    dc2 courses.dept_code%type;
+    cursor c1 is select * from students where stid = sid;
+    c1_rec c1%rowtype;
+    cursor c2 is select * from classes where classid = clid;
+    c2_rec c2%rowtype;
+    cursor c3 is select * from enrollments where sid = stid and classid = clid;
+    c3_rec c3%rowtype;
+    cursor c4 is select * from enrollments where sid = stid and classid in (select classid from classes where (dept_code,course_no) in (select dept_code,course_no from prerequisites where (pre_dept_code,pre_course_no) in (select dept_code,course_no from courses where (dept_code,course_no) in (select dept_code,course_no from classes where classid = clid))));
+    c4_rec c4%rowtype;
+    begin
+      if(not c1%isopen) then
+        open c1;
+      end if;
+      if(not c2%isopen) then
+        open c2;
+      end if;
+      if(not c3%isopen) then
+        open c3;
+      end if;
+      if(not c4%isopen) then
+        open c4;
+      end if;
+      fetch c1 into c1_rec;
+      fetch c2 into c2_rec;
+      fetch c3 into c3_rec;
+      fetch c4 into c4_rec;
+      if (c1%notfound) then
+        dbms_output.put_line('sid not found.');
+      elsif (c2%notfound) then
+        dbms_output.put_line('classid not found.');
+      elsif (c4%found) then
+        dbms_output.put_line('The drop is not permitted because another class uses it as a prerequisite.');
+      else
+        select course_no,dept_code into cnum,dc from classes where classid = clid;
+        select count(*) into num_classes from enrollments where sid = stid and classid != clid;
+        select class_size into num_students from classes where classid = clid;
+        if (num_classes = 0) then
+          dbms_output.put_line('The student is enrolled in no class.');
+        end if;
+        if (num_students - 1 = 0) then
+          dbms_output.put_line('The class now has no students.');
+        end if;
+        delete from enrollments where sid = stid and classid = clid;
+      end if;
+    end;
+  end;
+/
+show errors
+
+exec drop_student('B001','c0002');
+exec drop_student('B009','c0001');
+exec drop_student('B001','c0009');
+exec drop_student('B004','c0004');
+
+create or replace trigger trig3
+  before delete on students
+  declare
+    referencing old as deletedrow;
+  begin
+      delete from enrollments where sid = deletedrow.sid;
+  end;
+/
+
+show errors
+create or replace procedure delete_student(stid in students.sid%type) is begin
+  declare
+  cursor c1 is select * from students where sid = stid;
+  c1_rec c1%rowtype;
+  begin
+    if(not c1%isopen) then
+      open c1;
+    end if;
+    fetch c1 into c1_rec;
+    if (c1%notfound) then
+      dbms_output.put_line('sid is invalid.');
+    else
+      delete from students where sid = stid;
+    end if;
+  end;
+end;
+/
+exec delete_student('B009');
+insert into students values('B003','Princess','Peach','junior',2.2,'peachy@bing.edu');
+exec delete_student('B003');
+insert into enrollments values('B004','c0001','A');
+/*exec show_students;
+exec show_enrollments;
+
+create or replace trigger trig4
+  after insert or delete on
+*/
