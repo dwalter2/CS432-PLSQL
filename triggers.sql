cf4ca83cd8fe7aa019cfc0f141504ab2c5f4e4d5
dwalter2 <32689604+dwalter2@users.noreply.github.com>
2020-10-19 18:43:38 -0400

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,4 +1,4 @@
-create or replace trigger trig1
+/*create or replace trigger trig1
   after insert on logs
   declare
     cursor c1 is select lpad(logid, 7, '0') logid from logs;
@@ -15,21 +15,78 @@ create or replace trigger trig1
     close c1;
   end;
 /
+*/
+create or replace trigger trig10_4
+  before delete on enrollments
+    for each row
+    declare
+      counter number(7);
+      user_name varchar2(10);
+    begin
+      select count(*) into counter from logs;
+      select user into user_name from dual;
+      insert into logs values(counter,user_name,SYSTIMESTAMP,'enrollments','delete',:old.classid);
+    end;
+/
+show errors
+
+create or replace trigger trig10_3
+  before insert on enrollments
+    for each row
+    declare
+      counter number(7);
+      user_name varchar2(10);
+    begin
+      select count(*) into counter from logs;
+      select user into user_name from dual;
+      insert into logs values(counter,user_name,SYSTIMESTAMP,'enrollments','insert',:new.classid);
+    end;
+/
+show errors
+
+create or replace trigger trig10_2
+  before delete on students
+    for each row
+    declare
+      counter number(7);
+      user_name varchar2(10);
+    begin
+      select count(*) into counter from logs;
+      select user into user_name from dual;
+      insert into logs values(counter,user_name,SYSTIMESTAMP,'students','delete',:old.sid);
+    end;
+/
+show errors
 
 create or replace trigger trig10_1
   before insert on students
     for each row
     declare
+      counter number(7);
       user_name varchar2(10);
     begin
+      select count(*) into counter from logs;
       select user into user_name from dual;
-      insert into logs values(0,user_name,SYSTIMESTAMP,'students','insert',:new.sid);
+      insert into logs values(counter,user_name,SYSTIMESTAMP,'students','insert',:new.sid);
     end;
 /
 show errors
 -- need to get it to print the logid being 7 characters long -- issue resolved with lpad */
+create or replace trigger trig5
+  after insert on enrollments
+  for each row
+  begin
+    update classes set class_size = class_size + 1 where classid = :new.classid;
+  end;
+/
 
-
+create or replace trigger trig3
+  after delete on enrollments
+  for each row
+  begin
+    update classes set class_size = class_size - 1 where classid = :old.classid;
+  end;
+/
 
 create or replace trigger trig4
   before delete on students
